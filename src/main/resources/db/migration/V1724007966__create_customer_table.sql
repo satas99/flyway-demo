da31754dc6066f052e4c3dfaef58d25d33acc2b1
serhatats99@gmail.com <serhatats99@gmail.com>
2024-12-15 02:45:13 +0300

    email column not null control removed.

--- src/main/resources/db/migration/V1724007966__create_customer_table.sql
+++ src/main/resources/db/migration/V1724007966__create_customer_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS customer (
     id    BIGINT PRIMARY KEY,
     name  VARCHAR(255),
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255)
 );
 
 INSERT INTO customer (id, name, email) VALUES (1, 'Serhat', 'test@gmail.com');
